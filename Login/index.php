9df97447f7f7de113efbb56a2ad9b5380ed59e6b
dropchopdrop <job.hy.chung@gmail.com>
2016-02-21 17:54:49 +0800

    Create index.php

---
+++ Login/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p {text-align:center;}
+input
+</style>
+</head>
+<body>
+
+
+<p>Welcome to LMS</p>
+
+<form name="form" method="post" action="login.php">
+<table align="center">
+<tr>
+  <td>Username:</td>
+  <td><input type="text" name="Login"><td>
+</tr>
+<tr>
+  <td>Last name:</td>
+  <td><input type="password" name="Password"></td>
+</tr>
+</table>
+<p><input type="submit" name="button" value="Login" /></p>
+</form>
+
+</body>
+</html>
